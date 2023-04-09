9ca1a768dcb1930eb7d2ac6a5f149df930d8f2f1
Gmike47 <galezra20@gmail.com>
2023-04-09 14:14:31 +0300

    fix copy to clipboard message

--- javascript.js
+++ javascript.js
@@ -1,5 +1,17 @@
+const showmess = () => {
+    const copyMess = document.getElementsByClassName('message')[0];
+    copyMess.style.display = "block";
+}
+
+const unshowMess = () => {
+    const copyMess = document.getElementsByClassName('message')[0];
+    copyMess.style.display = "none";
+}
+
 function copyToClipboard() {
-    navigator.clipboard.writeText('@gmail.com').then(() => {alert("email copied")});
+    navigator.clipboard.writeText('@gmail.com');
+    showmess();
+    setTimeout(unshowMess, 2000)
 }
 
 //creates a function to copy email adress to clipboard, i.e. ctrl+c
